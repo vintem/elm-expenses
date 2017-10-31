f432f55d3adbc8fc0ca9b12602404175e3106ea5
Thiago Temple <ttemple@kinaxis.com>
2017-10-31 13:03:20 -0400

    das

--- src/Commands.elm
+++ src/Commands.elm
@@ -1,6 +1,7 @@
 module Commands exposing (..)
 
 import Http
+import Json.Encode as Encode
 import Json.Decode as Decode
 import Json.Decode.Pipeline exposing (decode, required)
 import Msgs exposing (Msg)
@@ -15,9 +16,45 @@ fetchPlayers =
         |> Cmd.map Msgs.OnFetchPlayers
 
 
+savePlayerCmd : Player -> Cmd Msg
+savePlayerCmd player =
+    savePlayerRequest player
+        |> Http.send Msgs.OnPlayerSave
+
+
 fetchPlayersUrl : String
 fetchPlayersUrl =
     "http://localhost:4000/players"
+
+
+savePlayerUrl : PlayerId -> String
+savePlayerUrl playerId =
+    "http://localhost:4000/players/" ++ playerId
+
+
+savePlayerRequest : Player -> Http.Request Player
+savePlayerRequest player =
+    Http.request
+        { body = playerEncoder player |> Http.jsonBody
+        , expect = Http.expectJson playerDecoder
+        , headers = []
+        , method = "PATCH"
+        , timeout = Nothing
+        , url = savePlayerUrl player.id
+        , withCredentials = False
+        }
+
+
+playerEncoder : Player -> Encode.Value
+playerEncoder player =
+    let
+        attributes =
+            [ ( "id", Encode.string player.id )
+            , ( "name", Encode.string player.name )
+            , ( "level", Encode.int player.level )
+            ]
+    in
+        Encode.object attributes
 
 
 playersDecoder : Decode.Decoder (List Player)
